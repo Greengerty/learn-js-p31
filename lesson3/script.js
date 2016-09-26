76161d2071cec2dd2f1a70b81d903076f5cd4ca9
Greengerty <greengerty@gmail.com>
2016-09-26 09:28:03 +0300

    lesson 3

---
+++ lesson3/script.js
@@ -0,0 +1,8 @@
+var socialMedia = {
+  youtube: 'http://youtube.com/planetoftheweb',
+  google: 'http://youtube.com/planetoftheweb',
+  facebook: 'http://youtube.com/planetoftheweb',
+  wordpress: 'http://youtube.com/planetoftheweb',
+  flickr: 'http://youtube.com/planetoftheweb',
+  meetup: 'http://youtube.com/planetoftheweb'
+};
